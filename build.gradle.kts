babe2268bc7db793b551e145f61f8d5e21b08254
alexsh <work.aleksandr.sh@gmail.com>
2024-01-28 22:24:54 +0600

    fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,11 @@ plugins {
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
     id("com.google.cloud.tools.jib") version "3.4.0"
-    id("nebula.release") version "19.0.4"
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.spring") version "1.9.22"
-    id("maven-publish")
 }
 
+version = "0.0.1"
 group = "com.example"
 
 java {
@@ -18,7 +17,6 @@ java {
 
 repositories {
     mavenCentral()
-    mavenLocal()
 }
 
 dependencies {
@@ -38,9 +36,7 @@ jib {
     }
     to {
         image = "hello-app"
-        project.afterEvaluate {
-            tags = setOf(project.version.toString())
-        }
+        tags = setOf(project.version.toString())
     }
 }
 
